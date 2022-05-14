222130cb7428668101f304c78554bcf35b3fee06
BasToTheMax <bas@arnold-linda.nl>
2022-05-14 18:06:08 +0200

    Beginning of database

---
+++ installdb.js
@@ -0,0 +1,40 @@
+const config = require('./config.json');
+
+const knex = require('knex')({
+    client: 'better-sqlite3',
+    connection: {
+      filename: __dirname + '/' + config.database
+    },
+    migrations: {
+        tableName: 'migrations'
+    },
+    log: {
+        warn(message) {
+            console.log('db - warn', message)
+        },
+        error(message) {
+            console.log('db - error', message)
+        },
+        deprecate(message) {
+            console.log('db - deprecate', message)
+        },
+        debug(message) {
+            console.log('db - debug', message)
+        },
+    }
+});
+
+function createUserTable() {
+    knex.schema.hasTable('users').then(function(exists) {
+        if (!exists) {
+        return knex.schema.createTable('users', function(t) {
+            t.increments('ID').primary();
+            t.string('username', 100);
+            t.string('password', 100);
+            t.string('email', 100);
+            t.string('role', 100); // user, reseller, staff, moderator, admin or owner
+            t.float('coins', 25, 500);
+        });
+        }
+    });
+}
---
+++ knexfile.js
@@ -0,0 +1,27 @@
+// Update with your config settings.
+
+const config = require('./config.json');
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'better-sqlite3',
+    connection: {
+      filename: __dirname + '/' + config.database
+    }
+  },
+
+  production: {
+    client: 'better-sqlite3',
+    connection: {
+      filename: __dirname + '/' + config.database
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
+};
