75482b45f293bf998fa59700fead87967daf9417
BasToTheMax <bas@arnold-linda.nl>
2022-05-14 18:19:44 +0200

    Install: Some improvements

--- installdb.js
+++ installdb.js
@@ -1,34 +1,37 @@
-const config = require('./config.json');
+(async () => {
+    const config = require('./config.json');
+    const chalk = require('chalk');
 
-const knex = require('knex')({
-    client: 'better-sqlite3',
-    connection: {
-      filename: __dirname + '/' + config.database
-    },
-    migrations: {
-        tableName: 'migrations'
-    },
-    log: {
-        warn(message) {
-            console.log('db - warn', message)
+    const knex = require('knex')({
+        client: 'better-sqlite3',
+        connection: {
+        filename: __dirname + '/' + config.database
         },
-        error(message) {
-            console.log('db - error', message)
+        useNullAsDefault: true,
+        migrations: {
+            tableName: 'migrations'
         },
-        deprecate(message) {
-            console.log('db - deprecate', message)
-        },
-        debug(message) {
-            console.log('db - debug', message)
-        },
-    }
-});
+        log: {
+            warn(message) {
+                console.log('db - warn', message)
+            },
+            error(message) {
+                console.log('db - error', message)
+            },
+            deprecate(message) {
+                console.log('db - deprecate', message)
+            },
+            debug(message) {
+                console.log('db - debug', message)
+            },
+        }
+    });
 
-// Functions
-function createUserTable() {
-    knex.schema.hasTable('users').then(function(exists) {
-        if (!exists) {
-            console.log('creating user table...')
+    // Functions
+    async function createUserTable() {
+        var exists = await knex.schema.hasTable('users');
+        if (exists == false) {
+            console.log('Creating user table...')
             return knex.schema.createTable('users', function(t) {
                 t.increments('ID').primary();
                 t.string('username', 100);
@@ -36,11 +39,19 @@ function createUserTable() {
                 t.string('email', 100);
                 t.string('role', 100); // user, reseller, staff, moderator, admin or owner
                 t.float('coins', 25, 500);
-                console.log('user table created!');
+                console.log(chalk.green('Users table created!'));
             });
+        } else {
+            console.log(chalk.red('Users table already exists! Are you trying to install on a existing instance?'));
         }
-    });
-}
+    }
 
-// Create tables
-createUserTable();
+    // Create tables
+    await createUserTable();
+
+    // Done
+    console.log(chalk.green('Installation complete!'));
+
+    // Exit
+    process.exit(0);
+})();
